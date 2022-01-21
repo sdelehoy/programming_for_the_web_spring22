265f5da1872281ebe5f35c62270646d5f9b0ce73
sdelehoy <sdelehoy@gmail.com>
2022-01-21 15:07:29 -0700

    Update snowman.js

--- wk1/assignments/snowman.js
+++ wk1/assignments/snowman.js
@@ -2,11 +2,19 @@ let grid = undefined;
 
 function setup() {
   createCanvas(1000,800);
-  background("#ccc");
+  background("#4ECDFF");
   grid = loadImage("images/100px_grid.png");
 }
 
 function draw() {
   background(grid);
+  
+  //ground
+  noStroke();
+  fill(255);
+  rect(0,532,999,268);
+  
+  //snow creature
   fill("#f1f1f1");
+  stroke(100, 250, 100);
 }
