64906eedca34fcdd17e62a1d5c1daf3a70cbae56
sdelehoy <sdelehoy@gmail.com>
2022-01-25 16:51:20 -0700

    Update games.js

--- wk2/exercises/ex2-2/games.js
+++ wk2/exercises/ex2-2/games.js
@@ -4,22 +4,26 @@ const games = [
     type: 'board game',
     numberOfPlayers: 4,
     rating: 7,
-    shortDescription: 'The Settlers of Catan is a multiplayer board game...Players take on the roles of settlers, each attempting to build and develop holdings while trading and acquiring resources. Players gain victory points as their settlements grow; the first to reach a set number of victory points, typically 10, wins. -Wikipedia'
+    shortDescription: 'In The Settlers of Catan players attempt to build settlements and collect resources.'
   },
   {
     title: 'Carcassonne',
     type: 'board game',
     numberOfPlayers: 5,
     rating: 8,
-    shortDescription: 'Carcassonne is a tile-based German-style board game for two to five players...The game board is a medieval landscape built by the players as the game progresses. -Wikipedia'
+    shortDescription: 'In Carcassonne the game board is a medieval landscape built by the players as the game progresses.'
   },
   {
     title: 'The Long Dark',
     type: 'video game',
-    numberOfPlayers: 4,
-    rating: 8,
-    shortDescription:
+    numberOfPlayers: 1,
+    rating: 6,
+    shortDescription: 'The Long Dark is a survival video game in an open world created with beautiful graphics. There is both a story mode as well as a survival mode.'
   }
 ];
-const num = window.prompt('I have 3 games in my collection. Pick a number between 1 and 3 and I will tell you about that game.');
-window.alert('You selected ' + ' which is a ' + '. Up to ' + ' people can play. It is rated as ' + ' out of 10.' + );
+
+const response = window.prompt('I have 3 games in my collection. Pick a number between 1 and 3 and I will tell you about that game.');
+
+const arrNum = response - 1;
+
+window.alert('You selected ' + games[arrNum].title + ' which is a ' + gmaes[arrNum].type + '. Up to ' + games[arrNum].numberOfPlayers + ' people can play. It is rated as ' + games[arrNum].rating + ' out of 10.' + games[arrNum].shortDescription);
