e4a101548938d4cd95ec5e887c256b43c641ef5a
sdelehoy <sdelehoy@gmail.com>
2022-01-25 16:13:29 -0700

    updated games.js

---
+++ wk2/exercises/ex2-2/games.js
@@ -0,0 +1,23 @@
+const games = [
+  {
+    title: 'The Settlers of Catan',
+    type: 'board game',
+    numberOfPlayers: 4,
+    rating: 7,
+    shortDescription: 'The Settlers of Catan is a multiplayer board game...Players take on the roles of settlers, each attempting to build and develop holdings while trading and acquiring resources. Players gain victory points as their settlements grow; the first to reach a set number of victory points, typically 10, wins. -Wikipedia'
+  },
+  {
+    title: 'Carcassonne',
+    type: 'board game',
+    numberOfPlayers: 5,
+    rating: 8,
+    shortDescription: 'Carcassonne is a tile-based German-style board game for two to five players...The game board is a medieval landscape built by the players as the game progresses. -Wikipedia'
+  },
+  {
+    title: 'The Long Dark',
+    type: 'video game',
+    numberOfPlayers: 4,
+    rating: 8,
+    shortDescription:
+  }
+];
