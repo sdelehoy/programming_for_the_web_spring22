779eb3b3c0c771100c0528ffc4b3482cfdf79f00
sdelehoy <sdelehoy@gmail.com>
2022-01-25 12:50:50 -0700

    Update movie.js

---
+++ wk2/exercises/ex2-1/movie.js
@@ -0,0 +1,4 @@
+const myFavMoives = ['Dumb and Dumber', 'Lord of the Rings', 'Gran Torino', ];
+const yourFavMovie = window.prompt('What is your favorite Movie?');
+myFavMoives.push(yourFavMovie);
+window.alert(myfavMoives);
