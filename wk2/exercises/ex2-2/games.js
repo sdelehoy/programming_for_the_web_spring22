0038f32623da62cc11aa9a272e530b36ff26337d
sdelehoy <sdelehoy@gmail.com>
2022-01-26 15:19:10 -0700

    Update games.js

--- wk2/exercises/ex2-2/games.js
+++ wk2/exercises/ex2-2/games.js
@@ -14,16 +14,30 @@ const games = [
     shortDescription: 'In Carcassonne the game board is a medieval landscape built by the players as the game progresses.'
   },
   {
-    title: 'The Long Dark',
-    type: 'video game',
-    numberOfPlayers: 1,
-    rating: 6,
-    shortDescription: 'The Long Dark is a survival video game in an open world created with beautiful graphics. There is both a story mode as well as a survival mode.'
+    title: 'Apples to Apples',
+    type: 'card game',
+    numberOfPlayers: 10,
+    rating: 8,
+    shortDescription: 'In Apples to Apples each player lays down a card with an noun they think the judge will determine best matches the adjective on the card the judge has laid down.'
+  },
+  {
+    title: 'Hide and Seek',
+    type: 'activity game',
+    numberOfPlayers: 10,
+    rating: 8,
+    shortDescription: 'In Hide and Seek one person hides and the other players try to find that person.'
+  },
+  {
+    title: 'Capture the Flag',
+    type: 'activity game',
+    numberOfPlayers: 20,
+    rating: 7,
+    shortDescription: 'In Capture the Flag players are divided into two teams. Each team gets a territory in which they place their flag and defend it from being taken.'
   }
 ];
 
-const response = window.prompt('I have 3 games in my collection. Pick a number between 1 and 3 and I will tell you about that game.');
+const response = window.prompt('I have 5 games in my collection. Pick a number between 1 and 5 and I will tell you about that game.');
 
-const arrNum = response - 1;
+const arrIndex = response - 1;
 
-window.alert('You selected ' + games[arrNum].title + ' which is a ' + games[arrNum].type + '. Up to ' + games[arrNum].numberOfPlayers + ' people can play. It is rated as ' + games[arrNum].rating + ' out of 10. ' + games[arrNum].shortDescription);
+window.alert('You selected ' + games[arrIndex].title + ' which is a ' + games[arrIndex].type + '. Up to ' + games[arrIndex].numberOfPlayers + ' people can play. I rate it as ' + games[arrIndex].rating + ' out of 10. ' + games[arrIndex].shortDescription);
