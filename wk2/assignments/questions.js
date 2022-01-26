4121e9e1a4a53cc6bf32f2c9e3f9c479fde6cb58
sdelehoy <sdelehoy@gmail.com>
2022-01-26 16:49:24 -0700

    Update questions.js

---
+++ wk2/assignments/questions.js
@@ -0,0 +1,22 @@
+questions [
+  {
+    question: 'In what year was the War of 1812?',
+    answer: '1812'
+  },
+  {
+    question: 'What color was George Washingtons white horse?',
+    answer: 'white'
+  },
+  {
+    question: 'Who is buried in Grants tomb?',
+    answer: 'Grant'
+  },
+  {
+    question: 'If a rooster lands directly on the peak of a barn and lays and egg which direction would the egg roll off the barn roof?',
+    answer: 'Roosters dont lay eggs'
+  },
+  {
+    question: 'If a plane crashed right on the border of the USA and Canada in which country would they bury the survivors?',
+    answer: '1812'
+  }
+]
