a3f8844dda978f080a776399f5f0e3bd5eb0079a
sdelehoy <sdelehoy@gmail.com>
2022-01-26 15:33:44 -0700

    Update games.js

--- wk2/exercises/ex2-2/games.js
+++ wk2/exercises/ex2-2/games.js
@@ -2,35 +2,35 @@ const games = [
   {
     title: 'The Settlers of Catan',
     type: 'board game',
-    numberOfPlayers: 4,
+    numberOfPlayers: '2-4',
     rating: 7,
     shortDescription: 'In The Settlers of Catan players attempt to build settlements and collect resources.'
   },
   {
     title: 'Carcassonne',
     type: 'board game',
-    numberOfPlayers: 5,
+    numberOfPlayers: '2-5',
     rating: 8,
     shortDescription: 'In Carcassonne the game board is a medieval landscape built by the players as the game progresses.'
   },
   {
     title: 'Apples to Apples',
     type: 'card game',
-    numberOfPlayers: 10,
+    numberOfPlayers: '4-10',
     rating: 8,
     shortDescription: 'In Apples to Apples each player lays down a card with an noun they think the judge will determine best matches the adjective on the card the judge has laid down.'
   },
   {
     title: 'Hide and Seek',
     type: 'activity game',
-    numberOfPlayers: 10,
+    numberOfPlayers: 'unlimited',
     rating: 8,
     shortDescription: 'In Hide and Seek one person hides and the other players try to find that person.'
   },
   {
     title: 'Capture the Flag',
     type: 'activity game',
-    numberOfPlayers: 20,
+    numberOfPlayers: 'unlimited',
     rating: 7,
     shortDescription: 'In Capture the Flag players are divided into two teams. Each team gets a territory in which they place their flag and defend it from being taken.'
   }
@@ -38,6 +38,6 @@ const games = [
 
 const response = window.prompt('I have 5 games in my collection. Pick a number between 1 and 5 and I will tell you about that game.');
 
-const arrIndex = response - 1;
+const arrIndex = Number(response) - 1;
 
-window.alert('You selected ' + games[arrIndex].title + ' which is a ' + games[arrIndex].type + '. Up to ' + games[arrIndex].numberOfPlayers + ' people can play. I rate it as ' + games[arrIndex].rating + ' out of 10. ' + games[arrIndex].shortDescription);
+window.alert('You selected ' + games[arrIndex].title + ' which is a ' + games[arrIndex].type + '. This game is for ' + games[arrIndex].numberOfPlayers + ' players. I rate it as ' + games[arrIndex].rating + ' out of 10. ' + games[arrIndex].shortDescription);
