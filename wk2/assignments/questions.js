cfd8b50a2c66161310d54d0d58e887f85806c075
sdelehoy <sdelehoy@gmail.com>
2022-01-26 17:42:21 -0700

    Update questions.js

--- wk2/assignments/questions.js
+++ wk2/assignments/questions.js
@@ -13,11 +13,11 @@ const questArr = [
   },
   {
     question: 'If a rooster lands directly on the peak of a barn and lays an egg which direction would the egg roll off the barn roof, right or left?',
-    answer: 'Roosters do not lay eggs'
+    answer: 'roosters do not lay eggs'
   },
   {
     question: 'If a plane crashed right on the border of the USA and Canada in which country would they bury the survivors?',
-    answer: 'Survivors are not buried'
+    answer: 'survivors are not buried'
   }
 ];
 
@@ -25,6 +25,6 @@ const randomNum = Math.round(Math.random() * (questArr.length - 1));
 
 const promptResponse = window.prompt(questArr[randomNum].question);
 
-const alertString = ''.concat('You answered ', promptResponse, '. The correct answer was ', questArr[randomNum].answer, '.');
+const alertString = ''.concat('You answered ', promptResponse, '. The correct answer is ', questArr[randomNum].answer, '.');
 
 window.alert(alertString);
