9fe50af549bb1fd445ac95282ffe1682f3f9322e
sdelehoy <sdelehoy@gmail.com>
2022-02-01 16:40:29 -0700

    Update tile.js

--- wk3/exercises/ex3-1/tile.js
+++ wk3/exercises/ex3-1/tile.js
@@ -2,10 +2,29 @@ function setup() {
   createCanvas(200, 200);
 }
 
+function polygon(x, y, radius, npoints) {
+  let angle = TWO_PI / npoints;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius;
+    let sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
+
 function createTile() {
   translate(0, 0);
-  fill('blue');
+  fill('purple');
+  noStroke();
   rect(0, 0, 200, 200);
+  fill('orange');
+  circle(100, 100, 100);
+  noFill();
+  stroke('magenta');
+  strokeWeight(3);
+  polygon(100, 100, 85, 6);
+  rotate(90);
 }
 
 function draw() {
