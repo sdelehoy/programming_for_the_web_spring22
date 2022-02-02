b2347118c432dba7f2fd48220fd41c54e64f11b0
sdelehoy <sdelehoy@gmail.com>
2022-02-01 17:02:02 -0700

    Update tile.js

--- wk3/exercises/ex3-1/tile.js
+++ wk3/exercises/ex3-1/tile.js
@@ -18,13 +18,27 @@ function createTile() {
   fill('purple');
   noStroke();
   rect(0, 0, 200, 200);
-  fill('orange');
-  circle(100, 100, 100);
+  
   noFill();
   stroke('magenta');
-  strokeWeight(3);
+  strokeWeight(4);
+  line(59, 171, 100, 100);
+  line(100, 100, 181, 100);
+  line(59, 29, 100, 100);
+  
+  noStroke();
+  fill('orange');
+  circle(100, 100, 100);
+  
+  noFill();
+  stroke('magenta');
+  strokeWeight(4);
   polygon(100, 100, 85, 6);
-  rotate(90);
+  line(18, 100, 100, 100);
+  line(100, 100, 140, 29);
+  line(100, 100, 141, 171);
+
+  
 }
 
 function draw() {
