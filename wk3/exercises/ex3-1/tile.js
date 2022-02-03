a589428041d0844013074e2acc989192b228504d
sdelehoy <sdelehoy@gmail.com>
2022-02-02 17:12:26 -0700

    Update tile.js

--- wk3/exercises/ex3-1/tile.js
+++ wk3/exercises/ex3-1/tile.js
@@ -19,17 +19,38 @@ function createTile() {
   noStroke();
   rect(0, 0, 200, 200);
   
+  noStroke();
+  fill('rgba(0, 255, 255, 0.4)');
+  beginShape();
+  vertex(59, 26);
+  vertex(142, 26);
+  vertex(185, 100);
+  vertex(142, 174);
+  vertex(59, 174);
+  vertex(16, 100);
+  
+  endShape();
+  
   noFill();
   stroke('magenta');
   strokeWeight(2);
   line(42, 200, 100, 100);
   line(100, 100, 200, 100);
-  line(59, 29, 100, 100);
+  line(42, 0, 100, 100);
   
   stroke('red');
   fill('orange');
   circle(100, 100, 100);
-  
+
+  noStroke();
+  fill('rgba(0, 255, 255, 0.4)');
+  //beginShape();
+  vertex(16, 100);
+  vertex(100, 100);
+  vertex(142, 174);
+  vertex(59, 174);
+  //endShape();
+
   noFill();
   stroke('magenta');
   strokeWeight(2);
@@ -37,7 +58,7 @@ function createTile() {
   
   line(0, 100, 100, 100);
   line(100, 100, 158, 0);
-  line(100, 100, 141, 171);
+  line(100, 100, 158, 200);
 }
 
 function draw() {
