35f913dcb1db1d0a02a2fe0456755b4d7f0b538c
sdelehoy <sdelehoy@gmail.com>
2022-02-04 15:42:25 -0700

    updated

--- wk3/assignments/quiz.js
+++ wk3/assignments/quiz.js
@@ -1,23 +1,38 @@
 const questArr = [
   {
-    question: 'In what year was the War of 1812?',
-    answer: '1812'
+    question: 'Is this a Black-capped Chickadee or an American Goldfinch?',
+    answer: 'Black-capped Chickadee',
+    image: 'images/black-capped_chickadee-720px.jpg'
   },
   {
-    question: 'What color was George Washingtons white horse?',
-    answer: 'white'
+    question: 'Is this an American Robin or an American Goldfinch',
+    answer: 'American Goldfinch',
+    image: 'images/black-capped_chickadee-720px.jpg'
   },
   {
-    question: 'Who is buried in Grants tomb?',
-    answer: 'Grant'
+    question: 'Is this an American Robin or a House Sparrow?',
+    answer: 'American Robin',
+    image: 'images/black-capped_chickadee-720px.jpg'
   },
   {
-    question: 'If a rooster lands directly on the peak of a barn and lays an egg which direction would the egg roll off the barn roof, right or left?',
-    answer: 'roosters do not lay eggs'
+    question: 'Is this a House Finch or a House Sparrow?',
+    answer: 'House Sparrow',
+    image: 'images/black-capped_chickadee-720px.jpg'
   },
   {
-    question: 'If a plane crashed right on the border of the USA and Canada in which country would they bury the survivors?',
-    answer: 'survivors are not buried'
+    question: 'Is this a House Finch or a Downy Woodpecker?',
+    answer: 'House Finch',
+    image: 'images/black-capped_chickadee-720px.jpg'
+  },
+  {
+    question: 'Is this a Blue Jay or a Downy Woodpecker?',
+    answer: 'Downy Woodpecker',
+    image: 'images/black-capped_chickadee-720px.jpg'
+  },
+  {
+    question: 'Is this a Blue Jay or a Black-capped Chickadee?',
+    answer: 'Blue Jay',
+    image: 'images/black-capped_chickadee-720px.jpg'
   }
 ];
 
@@ -31,21 +46,29 @@ let nextQuestion = function() {
 }
 
 let currentQuestion = nextQuestion().question;
+let currentAnswer = nextQuestion().answer;
+let currentImage = nextQuestion().image;
+
+let img;
+function preload() {
+  img = loadImage(currentImage);
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   let h1 = createElement('h1', 'Backyard Bird Quiz');
-  h1.position(300, 100);
+  h1.position(200, 50);
   let answerInput = createInput('');
-  answerInput.position(300, 300);
+  answerInput.position(200, 200);
   answerInput.size(250, 30);
   let submitAnswerBtn = createButton('Submit Answer');
-  submitAnswerBtn.position(300, 350);
+  submitAnswerBtn.position(200, 250);
   submitAnswerBtn.size(100, 30);
 }
 
 function draw() {
   background(240);
+  image(img, 200, 325);
   textSize(24);
-  text(currentQuestion, 300, 270);
+  text(currentQuestion, 200, 175);
 }
