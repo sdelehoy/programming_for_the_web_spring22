c249609d2f435f1c13d0973bfc7d19b179633bf3
sdelehoy <sdelehoy@gmail.com>
2022-02-04 13:01:37 -0700

    Update quiz.js

--- wk3/assignments/quiz.js
+++ wk3/assignments/quiz.js
@@ -25,7 +25,17 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(240);
   let h1 = createElement('h1', 'Backyard Bird Quiz');
-  h1.position(width/2 - 140, 200);
+  h1.position(300, 100);
+  let answerInput = createInput('');
+  answerInput.position(300, 300);
+  answerInput.size(250, 30);
+  let submitAnswerBtn = createButton('Submit Answer');
+  submitAnswerBtn.position(300, 350);
+  submitAnswerBtn.size(100, 30);
+}
+
+function draw() {
+
 }
 
 /* const randomNum = Math.round(Math.random() * (questArr.length - 1)); 
