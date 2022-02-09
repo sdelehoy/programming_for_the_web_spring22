60f9e7285ff5425556b9d46a3b7b701dac2a8c17
sdelehoy <sdelehoy@gmail.com>
2022-02-09 16:39:51 -0700

    Update exercise1.js

--- wk4/exercises/ex4-1/exercise1.js
+++ wk4/exercises/ex4-1/exercise1.js
@@ -1,4 +1,9 @@
 const myEmptyDiv = document.querySelector('#myEmptyDiv');
 const h1 = document.createElement('h1');
-h1.innerHTML = 'Llamas';
+h1.innerHTML = 'Llamas Make Great Trail Companions';
+h1.style.cursor = 'pointer';
+h1.addEventListener('click', handleHeadingClick);
 myEmptyDiv.appendChild(h1);
+function handleHeadingClick () {
+  document.body.style.backgroundColor = 'chartreuse';
+}
