1e7f3892438b31cb03e5f566f839e8d1892bb760
sdelehoy <sdelehoy@gmail.com>
2022-02-09 17:25:10 -0700

    Update exercise2.js

--- wk4/exercises/ex4-2/exercise2.js
+++ wk4/exercises/ex4-2/exercise2.js
@@ -1,8 +1,23 @@
+let cirX = 0;
+let cirY = 50;
+const cirWidth = 100;
+
 function setup () {
   createCanvas(500, 500);
 }
 
 function draw () {
-  fill('green');
-  cir(250, 250, 100);
+  background('olivedrab');
+  drawShape();
+  cirX += 1.5;
+  cirY++;
+}
+
+/* function mousePressed () {
+  if (mouseX, mouseY,)
+} */
+
+function drawShape () {
+  fill('cyan');
+  circle(cirX, cirY, cirWidth);
 }
