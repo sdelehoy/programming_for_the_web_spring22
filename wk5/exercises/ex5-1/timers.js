d1ee0efca309a4d3b4e5c95e27170c5ea70f7c18
sdelehoy <sdelehoy@gmail.com>
2022-02-15 14:00:06 -0700

    Update timers.js

--- wk5/exercises/ex5-1/timers.js
+++ wk5/exercises/ex5-1/timers.js
@@ -1,4 +1,32 @@
+let blockX = 0;
+let blockY = 0;
+let drawTimer;
+const speed = 20;
+const distance = 2;
+
 function setup () {
   createCanvas(500, 500);
   background(0);
 }
+
+function drawBlock(x, y, color) {
+  fill(color || 255);
+  square(x, y, 50);
+}
+
+function keyPressed() {
+  
+}
+
+drawTimer = window.setInterval(() => {
+  if(blockY - 50 <= height) {
+    drawBlock(blockX, blockY, 255);
+    blockY += distance;
+  } else {
+    blockY = 0;
+    blockX += 50;
+  }
+  if(blockY - 50 > height && blockX - 50 > width) {
+    window.clearInterval(drawTimer);
+  }
+}, speed);
