1f55935a02ac8dfd58aca1a8a4086d2424a35858
sdelehoy <sdelehoy@gmail.com>
2022-02-16 16:10:22 -0700

    Update memory.js

--- wk5/assignments/memory.js
+++ wk5/assignments/memory.js
@@ -2,19 +2,42 @@ let cards = [];
 let startX = 100;
 let startY = 150;
 let cardback;
+let cardfaces = [];
 
 function preload() {
-  cardback = loadImage(images/cardback.jpg);
+  cardback = loadImage('images/cardback.jpg');
+  cardfaces = [
+    loadImage('images/bluejay.jpg'),
+    loadImage('images/chickadee.jpg'),
+    loadImage('images/goldfinch.jpg'),
+    loadImage('images/housefinch.jpg'),
+    loadImage('images/robin.jpg'),
+    loadImage('images/woodpecker.jpg')
+  ]
 }
 
 function setup() {
   createCanvas(1150, 925);
   background('#273C17');
+  drawingContext.shadowOffsetX = 5;
+  drawingContext.shadowOffsetY = 5;
+  drawingContext.shadowBlur = 10;
+  drawingContext.shadowColor = 'black';
   fill('#121C0B');
   rect(0, 0, width, 75);
+  let selectedFaces = [];
+  for (let k = 0; k < 6; k++) {
+    const randomIdx = floor(random(cardfaces.length));
+    const face = cardfaces[randomIdx];
+    selectedFaces.push(face);
+    selectedFaces.push(face);
+    cardfaces.splice(randomIdx, 1);
+  }
+  selectedFaces = shuffleArray(selectedFaces);
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 4; j ++) {
-      cards.push(new Card(startX, startY));
+      const faceImage = selectedFaces.pop();
+      cards.push(new Card(startX, startY, faceImage));
       startX += 250;
     }
     startY += 250;
@@ -29,11 +52,12 @@ function mousePressed() {
 }
 
 class Card {
-  constructor(x, y) {
+  constructor(x, y, cardface) {
     this.x = x;
     this.y = y;
     this.width = 200;
     this.faceDown = true;
+    this.cardface = cardface;
     this.show();
   }
   show() {
@@ -45,6 +69,7 @@ class Card {
     } else {
       fill('cyan');
       square(this.x, this.y, this.width);
+      image(this.cardface, this.x, this.y);
       //this.faceDown = true;
     }
   }
@@ -60,4 +85,16 @@ class Card {
     }
     this.show();
   } */
+}
+
+function shuffleArray (array) {
+  let counter = array.length;
+  while (counter > 0) {
+    const idx = Math.floor(Math.random() * counter);
+    counter--;
+    const temp = array[counter];
+    array[counter] = array[idx];
+    array[idx] = temp;
+  }
+  return array;
 }
