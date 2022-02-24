b5844a1f43aba9082aa6ff63b095252fdc53d004
sdelehoy <sdelehoy@gmail.com>
2022-02-24 14:21:38 -0700

    update

---
+++ wk6/assignments/script.js
@@ -0,0 +1,17 @@
+new Vue({
+  el: '#app',
+  data: {
+    llamaFacts: [
+      { fact: '1. Llamas are actually from the same family as camels.' },
+      { fact: '2. Llamas are native to South America.' },
+      { fact: '3. Llamas do not live in the wild. They are domesticated animals.' },
+      { fact: '4. Llamas can spit distances of more than 15 feet!' },
+      { fact: '5. Llamas speak to each other by humming.' },
+      { fact: '6. Llamas are much more friendly than alpacas.' },
+      { fact: '7. Llama poop has almost no odor and is a fantastic fuel source for fires and great fertilizer.' },
+      { fact: '8. Herds of llamas have constantly shifting social structures. They have their own pecking order and males jostle for positions.' },
+      { fact: '9. Forget about guard dogs, you can have a guard llama. They make great guard animals for herds of sheep and alpaca.' },
+      { fact: '1. Llamas have now taken on the role of therapy animals.' }
+    ]
+  }
+})
