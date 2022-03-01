4675cf353910cdc76760d252928ddc2e8342154f
sdelehoy <sdelehoy@gmail.com>
2022-03-01 14:14:13 -0700

    update

--- wk7/exercises/ex7-1/script.js
+++ wk7/exercises/ex7-1/script.js
@@ -4,6 +4,7 @@ Vue.createApp({
       message: 'Hello Vue!',
       wikiLlama: 'https://en.wikipedia.org/wiki/Llama',
       wikiAlpaca: 'https://en.wikipedia.org/wiki/Alpaca',
+      britannica: 'https://www.britannica.com/story/whats-the-difference-between-llamas-and-alpacas'
     }
   }
 }).mount('#app')
