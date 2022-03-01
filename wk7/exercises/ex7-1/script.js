35d19ae9f2dcfb9a208104f2468852f28430d103
sdelehoy <sdelehoy@gmail.com>
2022-03-01 13:57:41 -0700

    update

--- wk7/exercises/ex7-1/script.js
+++ wk7/exercises/ex7-1/script.js
@@ -1,7 +1,9 @@
 Vue.createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      wikiLlama: 'https://en.wikipedia.org/wiki/Llama',
+      wikiAlpaca: 'https://en.wikipedia.org/wiki/Alpaca',
     }
   }
 }).mount('#app')
