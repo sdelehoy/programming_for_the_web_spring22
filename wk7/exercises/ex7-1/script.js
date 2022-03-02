753d7f3d406aaf2af00b83768f9adaba74a27b91
sdelehoy <sdelehoy@gmail.com>
2022-03-02 12:21:07 -0700

    update

--- wk7/exercises/ex7-1/script.js
+++ wk7/exercises/ex7-1/script.js
@@ -7,6 +7,8 @@ Vue.createApp({
       alpacaTitle: 'Wikipedia article on alpacas',
       britannica: 'https://www.britannica.com/story/whats-the-difference-between-llamas-and-alpacas',
       britannicaTitle: 'What\'s the Difference Between Llamas and Alpacas',
+      newWindow: '_blank',
+      security: 'noopener noreferrer',
       textOnBackground: {
         color: 'hsl(0, 0%, 100%)',
         'text-shadow': '0px 0px 25px hsla(0, 0%, 0%, 0.2)'
