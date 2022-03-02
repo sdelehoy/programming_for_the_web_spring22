76c47025de4d2b1e7d02014ab0d5d8c205ba47b6
sdelehoy <sdelehoy@gmail.com>
2022-03-02 11:40:23 -0700

    update

--- wk7/exercises/ex7-1/script.js
+++ wk7/exercises/ex7-1/script.js
@@ -6,7 +6,11 @@ Vue.createApp({
       wikiAlpaca: 'https://en.wikipedia.org/wiki/Alpaca',
       alpacaTitle: 'Wikipedia article on alpacas',
       britannica: 'https://www.britannica.com/story/whats-the-difference-between-llamas-and-alpacas',
-      britannicaTitle: 'What\'s the Difference Between Llamas and Alpacas'
+      britannicaTitle: 'What\'s the Difference Between Llamas and Alpacas',
+      textOnBackground: {
+        color: 'hsl(0, 0%, 100%)',
+        'text-shadow': '0px 0px 25px hsla(0, 0%, 0%, 0.2)'
+      }
     }
   }
 }).mount('#app')
