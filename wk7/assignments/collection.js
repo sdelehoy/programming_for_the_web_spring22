c7036c58d48074052066eb151c5396c7c9eb8347
sdelehoy <sdelehoy@gmail.com>
2022-03-03 20:33:02 -0700

    update

--- wk7/assignments/collection.js
+++ wk7/assignments/collection.js
@@ -8,25 +8,29 @@ Vue.createApp({
           date: 'August 24, 2018',
           time: '1:00 pm',
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Cedar_Waxwing_-_Bombycilla_cedrorum%2C_George_Washington%27s_Birthplace_National_Monument%2C_Colonial_Beach%2C_Virginia_%2839997434862%29.jpg/162px-Cedar_Waxwing_-_Bombycilla_cedrorum%2C_George_Washington%27s_Birthplace_National_Monument%2C_Colonial_Beach%2C_Virginia_%2839997434862%29.jpg'
-        }, {
+        },
+        {
           bird: 'Mourning Dove',
           location: 'Dixon Reservoir',
           date: 'September 7, 2018',
           time: '12:56 pm',
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Mourning_Dove_2006.jpg/160px-Mourning_Dove_2006.jpg'
-        }, {
+        },
+        {
           bird: 'Blue Jay',
           location: 'Horsetooth Reservoir',
           date: 'September 14, 2018',
           time: '7:23 am',
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Blue_jay_in_PP_%2830960%29.jpg/176px-Blue_jay_in_PP_%2830960%29.jpg'
-        }, {
+        },
+        {
           bird: 'Canyon Wren',
           location: 'Horsetooth Reservoir',
           date: 'September 14, 2018',
           time: '7:24 am',
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Canyon_Wren.jpg/177px-Canyon_Wren.jpg'
-        }, {
+        },
+        {
           bird: 'Steller\'s Jay',
           location: 'Horsetooth Reservoir',
           date: 'November 17, 2018',
