c6e847ca39a8c1f360eb8a9e9c82ff695c2f1552
sdelehoy <sdelehoy@gmail.com>
2022-03-07 19:51:06 -0700

    update

--- wk8/exercises/ex8-1/script.js
+++ wk8/exercises/ex8-1/script.js
@@ -7,7 +7,25 @@ Vue.createApp({
       city: '',
       state: '',
       zipcode: '',
-      colors: [],
+      selectedColors: [],
+      colors: [
+        { name: 'Beige', value: 'beige'},
+        { name: 'Black - Bay', value: 'black-bay'},
+        { name: 'Black - True', value: 'black-true'},
+        { name: 'Brown - Light', value: 'brown-lt'},
+        { name: 'Brown - Medium', value: 'brown-md'},
+        { name: 'Brown - Dark', value: 'brown-dk'},
+        { name: 'Fawn - Light', value: 'fawn-lt'},
+        { name: 'Fawn - Medium', value: 'fawn-md'},
+        { name: 'Fawn - Dark', value: 'fawn-dk'},
+        { name: 'Rose Grey - Light', value: 'rose-grey-lt'},
+        { name: 'Rose Grey - Medium', value: 'rose-grey-md'},
+        { name: 'Rose Grey - Dark', value: 'rose-grey-dk'},
+        { name: 'Silver Grey - Light', value: 'silver-grey-lt'},
+        { name: 'Silver Grey - Medium', value: 'silver-grey-md'},
+        { name: 'Silver Grey - Dark', value: 'silver-grey-dk'},
+        { name: 'White', value: 'white'}
+      ],
       status: ''
     }
   }
