2f77606ce355c1f7ca09896026ff0430c7f32dad
sdelehoy <sdelehoy@gmail.com>
2022-03-09 16:06:17 -0700

    update

--- wk8/exercises/ex8-2/script.js
+++ wk8/exercises/ex8-2/script.js
@@ -9,6 +9,23 @@ Vue.createApp({
   },
   methods: {
     calculate(num1, num2) {
+      switch(this.operation) {
+        case 'add':
+          this.total = num1 + num2;
+          break;
+        case 'subtract':
+          this.total = num1 - num2;
+          break;
+        case 'multiply':
+          this.total = num1 * num2;
+          break;
+        case 'divide':
+          this.total = num1 / num2;
+          break;
+        default:
+          window.alert('unexpected operator');
+          break;
+      }
       this.firstNum = this.secondNum = null;
     }
   }
