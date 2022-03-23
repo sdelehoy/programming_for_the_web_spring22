e592943bc9a3540057b2b893cd51ce306a6596e1
sdelehoy <sdelehoy@gmail.com>
2022-03-23 11:22:57 -0600

    update

--- wk9/exercises/ex9-1/computed_props/lib/script.js
+++ wk9/exercises/ex9-1/computed_props/lib/script.js
@@ -13,5 +13,10 @@ Vue.createApp({
       state: "Minnesota",
       postalCode: "55404"
     }
+  },
+  computed: {
+    fullAddress() {
+      return this.streetAddress + ', ' + this.city + ', ' + this.state + ', ' + this.postalCode
+    }
   }
 }).mount("#app");
