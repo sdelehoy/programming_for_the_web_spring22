3405eb61a8b28aca2f36236166e9132e4de2f91a
sdelehoy <sdelehoy@gmail.com>
2022-03-24 14:16:55 -0600

    update

---
+++ wk9/exercises/ex9-2/src/App.vue
@@ -0,0 +1,58 @@
+<script setup>
+import CamelidRow from "./components/CamelidRow.vue";
+const camelids = [
+  {
+    id: 0,
+    commonName: "Lama",
+    binomialName: "Lama glama",
+    order: "Artiodactyla",
+    family: "Camelidae",
+    genus: "Lama",
+    species: "L. glama",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Llama_lying_down.jpg/220px-Llama_lying_down.jpg",
+  },
+  {
+    id: 1,
+    commonName: "Alpaca",
+    binomialName: "Vicugna pacos",
+    order: "Artiodactyla",
+    family: "Camelidae",
+    genus: "Vicugna",
+    species: "V. pacos",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Alpaca_%2831562329701%29.jpg/440px-Alpaca_%2831562329701%29.jpg",
+  },
+];
+</script>
+
+<template>
+  <div>
+    <h1>Camelids</h1>
+    <div class="container">
+      <CamelidRow
+        v-for="animal in camelids"
+        v-bind:key="animal.id"
+        v-bind:camelid="animal"
+      />
+    </div>
+  </div>
+</template>
+
+<style>
+h1 {
+  text-align: center;
+  margin-top: 50px;
+  text-transform: uppercase;
+  letter-spacing: 0.6rem;
+}
+div {
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  gap: 30px;
+  margin-top: 50px;
+}
+</style>
---
+++ wk9/exercises/ex9-2/src/components/CamelidRow.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps(["camelid"]);
+</script>
+
+<template>
+  <div>
+    <h2>{{ props.camelid.commonName }}</h2>
+    <img :src="props.camelid.image" :alt="props.camelid.commonName" />
+    <p>
+      <span class="label">Binomial Name:</span> {{ props.camelid.binomialName }}
+    </p>
+    <p><span class="label">Order:</span> {{ props.camelid.order }}</p>
+    <p><span class="label">Family:</span> {{ props.camelid.family }}</p>
+    <p><span class="label">Genus:</span> {{ props.camelid.genus }}</p>
+    <p><span class="label">Species:</span> {{ props.camelid.species }}</p>
+  </div>
+</template>
+
+<style scoped>
+img {
+  max-width: 200px;
+}
+.label {
+  font-weight: bold;
+}
+div {
+  background-color: hsl(182, 94%, 87%);
+  border-radius: 15px;
+  width: 300px;
+  padding: 20px;
+  box-shadow: 3px 3px 10px hsla(0, 0%, 0%, 0.2);
+}
+</style>
