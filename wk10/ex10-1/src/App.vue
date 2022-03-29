8943354ba43be491dd17889937d02bd551a3fabc
sdelehoy <sdelehoy@gmail.com>
2022-03-29 15:47:29 -0600

    update

---
+++ wk10/ex10-1/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/alpaca">Alpaca</RouterLink>
+      <RouterLink to="/llama">Llama</RouterLink>
+    </nav>
+    <div class="container">
+      <RouterView></RouterView>
+    </div>
+  </div>
+</template>
+
+<style></style>
---
+++ wk10/ex10-1/src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Alpaca from "../views/Alpaca.vue";
+import Llama from "../views/Llama.vue";
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/alpaca",
+      name: "alpaca",
+      component: Alpaca,
+    },
+    {
+      path: "/llama",
+      name: "llama",
+      component: Llama,
+    },
+  ],
+});
+
+export default router;
