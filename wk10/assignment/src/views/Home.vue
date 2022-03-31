589f56fbb0fe1b38d6d7945f66c7f02554a2732e
sdelehoy <sdelehoy@gmail.com>
2022-03-31 16:51:23 -0600

    Update Home.vue

--- wk10/assignment/src/views/Home.vue
+++ wk10/assignment/src/views/Home.vue
@@ -5,13 +5,38 @@ import SightingRow from "../components/SightingRow.vue";
 
 <template>
   <div>
-    <h1>Bird Sightings</h1>
-    <SightingRow
-      v-for="(sighting, index) in SightingsList"
-      :key="index"
-      :sighting="sighting"
-    />
+    <div class="title-container">
+      <h1>Bird Sightings</h1>
+    </div>
+    <div class="row-container">
+      <SightingRow
+        v-for="(sighting, index) in SightingsList"
+        :key="index"
+        :sighting="sighting"
+      />
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.title-container {
+  height: 80px;
+  background-color: hsl(0, 0%, 20%);
+  text-align: center;
+}
+.title-container h1 {
+  margin: 0;
+  line-height: 2.5;
+  color: hsl(0, 0%, 90%);
+  font-weight: lighter;
+  text-transform: uppercase;
+  letter-spacing: 0.2ch;
+}
+.row-container {
+  background-color: hsl(0, 0%, 100%);
+  width: 800px;
+  margin: 30px auto;
+  padding: 20px;
+  border-radius: 10px;
+}
+</style>
