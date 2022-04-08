7251647246851ae196a683c72bfac42df9043bb7
sdelehoy <sdelehoy@gmail.com>
2022-04-07 20:17:53 -0600

    Update iteration.js

--- wk11/exercises/ex11-2/iteration.js
+++ wk11/exercises/ex11-2/iteration.js
@@ -1,15 +1,27 @@
+angle = 0;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  fill(255);
-  stroke(102);
+  colorMode(HSB);
 }
 
 function draw() {
   background(0);
-  for (var y = 20; y <= height - 20; y += 10) {
-    for (var x = 20; x <= width - 20; x += 10) {
-      line(x, y, 240, 60);
-      circle(x, y, 4);
-    }
+  noStroke();
+  translate(width / 2, height / 2);
+  rotate(angle);
+  angle -= radians(0.1);
+  const n = 13;
+  const d = 14;
+  const k = n / d;
+  beginShape();
+  for (let a = 0; a < TWO_PI * d; a += 0.01) {
+    const r = 400 * cos(k * a);
+    const x = r * cos(a);
+    const y = r * sin(a);
+    const h = a * 5;
+    fill(h, 100, 60);
+    circle(x, y, 3);
   }
+  endShape(CLOSE);
 }
