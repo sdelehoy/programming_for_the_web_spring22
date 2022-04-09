fd40984d7122869c93aba0806fa33750456266d7
sdelehoy <sdelehoy@gmail.com>
2022-04-09 11:14:03 -0600

    Update script.js

--- wk11/assignments/11.A-1/script.js
+++ wk11/assignments/11.A-1/script.js
@@ -6,5 +6,12 @@ function setup() {
 }
 
 function draw() {
-
+  stroke(0, 0, 100);
+  const numColumns = 800;
+  const numRows = 500;
+  for (let x = 0; x <= width; x+= 10) {
+    for (let y = 0; y <= height; y+= 10) {
+      line(x, y, x + 7, y + 7);
+    }
+  }
 }
