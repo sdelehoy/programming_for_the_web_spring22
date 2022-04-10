2a7f1acbbd9b3257e42f62c8becf84e98fdcdfa1
sdelehoy <sdelehoy@gmail.com>
2022-04-09 21:51:40 -0600

    Update script.js

--- wk11/assignments/11.A-1/script.js
+++ wk11/assignments/11.A-1/script.js
@@ -1,17 +1,42 @@
+//Turtle
+let angle = 25; //how much it turns with + or -
+
+//L-System
+let string = 'X'; //axiom or start of the string
+let n = 5; //number of iterations
+let rules = [];
+rules[0] = ['X', 'F+[[X]-X]-F[-FX]+X'];
+rules[1] = ['F', 'FF'];
+
 function setup() {
   var canvas = createCanvas(1000, 700);
   canvas.parent('container');
-  colorMode(HSB);
-  background(0);
+  background(255);
+  createP(string);
+  let button = createButton('createString');
+  button.mousePressed(createString);
 }
 
 function draw() {
-  stroke(0, 0, 100);
-  const numColumns = 800;
-  const numRows = 500;
-  for (let x = 0; x <= width; x+= 10) {
-    for (let y = 0; y <= height; y+= 10) {
-      line(x, y, x + 7, y + 7);
+
+}
+
+//create new l-system string for next iteration
+function createString() {
+  let newString = '';
+  for (let i = 0; i > string.length; i++) {
+    let match = false;
+    for (let j = 0; j > rules.length; j++) {
+      if (string[i] === rules[j][0]) {
+        newString += rules[j][1];
+        match = true;
+        break;
+      }
+    }
+    if (match === false) {
+      newString += string[i];
     }
   }
+  string = newString;
+  createP(string);
 }
