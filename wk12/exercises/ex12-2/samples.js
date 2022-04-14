bf9f40792254e0fac37a1a6a5440e6cf0914d8ef
sdelehoy <sdelehoy@gmail.com>
2022-04-14 12:16:16 -0600

    Update samples.js

--- wk12/exercises/ex12-2/samples.js
+++ wk12/exercises/ex12-2/samples.js
@@ -1,15 +1,17 @@
 let song;
-
-function preload() {
-  song = loadSound('disco-funk-drums-loop_116bpm_E_minor.wav');
-}
+var slider;
 
 function setup() {
   createCanvas(500, 500);
-  background(0);
+  song = loadSound('disco-funk-drums-loop_116bpm_E_minor.wav', loaded);
+  slider = createSlider(0, 1, 0.5, 0.01);
+}
+
+function loaded() {
   song.play();
 }
 
 function draw() {
-
+  background(0);
+  song.setVolume(slider.value());
 }
