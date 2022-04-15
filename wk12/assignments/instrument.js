e935b56fd1b0459c21ad53bc6486007f607bc4ba
sdelehoy <sdelehoy@gmail.com>
2022-04-15 13:19:29 -0600

    Update instrument.js

--- wk12/assignments/instrument.js
+++ wk12/assignments/instrument.js
@@ -1,7 +1,25 @@
-function setup() {
+let polySynth;
 
+function setup() {
+  let cnv = createCanvas(100, 100);
+  cnv.mousePressed(playSynth);
+  background(0);
+  polySynth = new p5.PolySynth();
 }
 
 function draw() {
-  
+
+}
+
+function playSynth() {
+  userStartAudio();
+  let note = 'G4';
+  let velocity = 1;
+  let time = 0;
+  let dur = 1;
+  polySynth.noteAttack(note);
+}
+
+function mouseReleased() {
+  polySynth.noteRelease();
 }
