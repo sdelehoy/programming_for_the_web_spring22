7f63b2490ce6603cbf602a35fdb4755724949646
sdelehoy <sdelehoy@gmail.com>
2022-04-15 17:33:13 -0600

    update

--- wk12/assignments/instrument.js
+++ wk12/assignments/instrument.js
@@ -1,22 +1,11 @@
-let polySynth;
+
 
 function setup() {
   let cnv = createCanvas(100, 100);
-  cnv.mousePressed(playSynth);
+  cnv.mousePressed();
   background(0);
-  polySynth = new p5.PolySynth();
 }
 
 function draw() {
 
-}
-
-function playSynth() {
-  userStartAudio();
-  let note = 'Fb4';
-  polySynth.noteAttack(note);
-}
-
-function mouseReleased() {
-  polySynth.noteRelease();
 }
