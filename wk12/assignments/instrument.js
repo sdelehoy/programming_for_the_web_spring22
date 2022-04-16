cf9d79a269b7e29b3730cabbeee5b59e848330d2
sdelehoy <sdelehoy@gmail.com>
2022-04-16 12:51:17 -0600

    update

--- wk12/assignments/instrument.js
+++ wk12/assignments/instrument.js
@@ -1,11 +1,39 @@
+let kickButton;
+let snareButton;
+let hihatButton;
 
+let kickSound;
+let snareSound;
+let hihatSound;
 
 function setup() {
   let cnv = createCanvas(100, 100);
-  cnv.mousePressed();
   background(0);
+  kickButton = createButton('play kick');
+  kickButton.mousePressed(playKick);
+  snareButton = createButton('play snare');
+  snareButton.mousePressed(playSnare);
+  hihatButton = createButton('play hi hat');
+  hihatButton.mousePressed(playHihat);
 }
 
 function draw() {
+
+}
+
+function playKick() {
+  if (kickSound.isPlaying) {
+    kickSound.stop();
+    kickButton.html('play kick')
+  } else
+    kickSound.loop();
+    kickButton.html('stop kick');
+}
+
+function playSnare() {
+
+}
+
+function playHihat() {
 
 }
