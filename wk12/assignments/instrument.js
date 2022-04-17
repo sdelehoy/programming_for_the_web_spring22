c07769809fc3cc629e1b673cd857d8157bb7e28a
sdelehoy <sdelehoy@gmail.com>
2022-04-16 18:01:00 -0600

    update

--- wk12/assignments/instrument.js
+++ wk12/assignments/instrument.js
@@ -18,42 +18,56 @@ let tomSound;
 let tomButton;
 let tomSlider;
 
-function setup() {
-  createCanvas(100, 100);
-
+function preload() {
   kickSound1 = loadSound('sounds/Kick_V_10.wav');
-  kickButton1 = createButton('play');
-  kickButton1.mousePressed(playKick1);
-  kickSlider1 = createSlider(0.5, 2, 1, 0.25);
-
   kickSound2 = loadSound('sounds/Kick_V_10.wav');
-  kickButton2 = createButton('play kick 2');
-  kickButton2.mousePressed(playKick2);
-  kickSlider2 = createSlider(0.5, 2, 1, 0.25);
-
   snareSound = loadSound('sounds/Snare-1_V_9.wav');
-  snareButton = createButton('play snare');
-  snareButton.mousePressed(playSnare);
-  snareSlider = createSlider(0.5, 2, 1, 0.25);
-  
   hihatSound = loadSound('sounds/Closed-Shank.wav');
+  tomSound = loadSound('sounds/Floor-Tom_V_10.wav');
+}
+
+function setup() {
+  let cnv = createCanvas(100, 100);
+  cnv.parent('canvas');
+
+  kickButton1 = createButton('play kick 1');
+  kickButton1.parent('kick-button-1');
+  kickButton1.mousePressed(playKick1);
+  kickSlider1 = createSlider(0.25, 2, 1, 0.25);
+  kickSlider1.parent('kick-slider-1');
+
+  kickButton2 = createButton('play kick 2');
+  kickButton2.parent('kick-button-2');
+  kickButton2.mousePressed(playKick2);
+  kickSlider2 = createSlider(0.25, 2, 1, 0.25);
+  kickSlider2.parent('kick-slider-2');
+
+  snareButton = createButton('play snare');
+  snareButton.parent('snare-button');
+  snareButton.mousePressed(playSnare);
+  snareSlider = createSlider(0.25, 2, 1, 0.25);
+  snareSlider.parent('snare-slider');
+  
   hihatButton = createButton('play hi hat');
+  hihatButton.parent('hihat-button');
   hihatButton.mousePressed(playHihat);
   hihatSlider = createSlider(0.25, 2, 1, 0.25);
+  hihatSlider.parent('hihat-slider');
 
-  tomSound = loadSound('sounds/Floor-Tom_V_10.wav');
   tomButton = createButton('play tom');
+  tomButton.parent('tom-button');
   tomButton.mousePressed(playTom);
   tomSlider = createSlider(0.25, 2, 1, 0.25);
+  tomSlider.parent('tom-slider');
 }
 
 function draw() {
   background(255);
-  text(kickSlider1.value(), 10, 10);
-  text(kickSlider2.value(), 10, 30);
-  text(snareSlider.value(), 10, 50);
-  text(hihatSlider.value(), 10, 70);
-  text(tomSlider.value(), 10, 90);
+  text('kick slider 1: ' + kickSlider1.value(), 10, 10);
+  text('kick slider 2: ' + kickSlider2.value(), 10, 30);
+  text('snare slider: ' + snareSlider.value(), 10, 50);
+  text('hi hat slider: ' + hihatSlider.value(), 10, 70);
+  text('tom slider: ' + tomSlider.value(), 10, 90);
 }
 
 function playKick1() {
